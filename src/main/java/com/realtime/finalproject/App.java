fd023b7fb2f5e5fc3829b5c3c0669fad5806e692
Lee <lxm016400@utdallas.edu>
2017-06-24 21:50:38 -0500

    ss

---
+++ src/main/java/com/realtime/finalproject/App.java
@@ -0,0 +1,13 @@
+package com.realtime.finalproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
